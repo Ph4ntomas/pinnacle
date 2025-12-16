b9634113300cc3efeee813b2287856adbdd2f33e
phantomas <phantomas@phantomas.xyz>
2025-12-16 09:24:57 +0100

    snowcap: widget/container: Add user-defined id to Container

--- snowcap/src/api/widget/v1.rs
+++ snowcap/src/api/widget/v1.rs
@@ -364,6 +364,7 @@ pub fn widget_def_to_fn(def: WidgetDef) -> Option<ViewFn> {
                 clip,
                 child,
                 style,
+                id,
             } = *container_def;
 
             let child_widget_fn = child.and_then(|def| widget_def_to_fn(*def));
@@ -439,6 +440,10 @@ pub fn widget_def_to_fn(def: WidgetDef) -> Option<ViewFn> {
                 };
 
                 container = container.style(style);
+
+                if let Some(id) = id.clone() {
+                    container = container.id(id);
+                }
 
                 container.into()
             });
