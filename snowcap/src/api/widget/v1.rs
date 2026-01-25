6129901e800fd53883d861fa443ddc68ccde08a3
phantomas <phantomas@phantomas.xyz>
2026-01-25 14:54:24 +0100

    snowcap: wlr_task_list: Add operation to control tasks

--- snowcap/src/api/widget/v1.rs
+++ snowcap/src/api/widget/v1.rs
@@ -2,6 +2,7 @@ use anyhow::Context;
 use iced::widget::{
     Column, Container, Row, Scrollable, button, image::FilterMethod, scrollable::Scrollbar,
 };
+use smithay_client_toolkit::reexports::client::{Proxy, protocol::wl_seat::WlSeat};
 use snowcap_api_defs::snowcap::widget::{
     self,
     v1::{
@@ -1012,6 +1013,7 @@ pub fn widget_def_to_fn(def: WidgetDef, state: &State) -> Option<ViewFn> {
 
             let child_widget_fn = child.and_then(|def| widget_def_to_fn(*def, state));
             let wlr_state = state.zwlr_foreign_toplevel_mgmt_state.clone();
+            let seat = state.seat.as_ref().map(WlSeat::downgrade);
 
             let f: ViewFn = Box::new(move || {
                 let mut wlr_task_list = wlr_tasklist::WlrTaskList::new(
@@ -1020,6 +1022,7 @@ pub fn widget_def_to_fn(def: WidgetDef, state: &State) -> Option<ViewFn> {
                         .map(|child| child())
                         .unwrap_or_else(|| iced::widget::Row::new().into()),
                     wlr_state.clone(),
+                    seat.clone(),
                 );
 
                 if let Some(widget_id) = widget_id {
